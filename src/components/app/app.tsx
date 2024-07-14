7e563dfdff6351f38ed14a56283736e3abe4c2cb
Irina Lagutina <ivl77@yandex.ru>
2024-07-14 16:59:17 +0500

    Создали страницы избранное, логин, оффера

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,7 @@
 import MainPage from '../../pages/main-page/main-page';
+// import FavoritesPage from '../../pages/favorites-page/favorites-page';
+// import LoginPage from '../../pages/login-page/login-page';
+// import OfferPage from '../../pages/offer-page/offer-page';
 
 type AppProps = {
   foundOffers: number;
