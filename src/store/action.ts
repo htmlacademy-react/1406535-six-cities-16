7d2ad3b78c0bd1d7cd5eaa9fdf027c5ff20463f4
Irina Lagutina <ivl77@yandex.ru>
2024-08-26 16:22:12 +0300

    Добавили признак аутентификации в локальное состояние

--- src/store/action.ts
+++ src/store/action.ts
@@ -1,5 +1,7 @@
 import { City, Offer } from '../types';
 import { createAction } from '@reduxjs/toolkit';
+import { AuthorizationStatus } from '../const';
 
 export const setCity = createAction<City>('all/setCity');
-export const setOffers = createAction<Offer[]>('all/setOffers');
+export const fillOffers = createAction<Offer[]>('all/fillOffers');
+export const setAuthorization = createAction<AuthorizationStatus>('user/setAuthStatus');
--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,11 +1,12 @@
 import { Offer, City } from '../types';
 import { createReducer, PayloadAction } from '@reduxjs/toolkit';
-import { setCity, setOffers } from './action';
-import { DEFAULT_CITY } from '../const';
+import { setCity, fillOffers, setAuthorization } from './action';
+import { DEFAULT_CITY, AuthorizationStatus } from '../const';
 
 const initialState = {
   city: DEFAULT_CITY,
   offers: [] as Offer[],
+  authStatus: AuthorizationStatus.Unknown,
 };
 
 export const reducer = createReducer(initialState, (builder) => {
@@ -13,7 +14,10 @@ export const reducer = createReducer(initialState, (builder) => {
     .addCase(setCity, (state, action: PayloadAction<City>) => {
       state.city = action.payload;
     })
-    .addCase(setOffers, (state, action: PayloadAction<Offer[]>) => {
+    .addCase(fillOffers, (state, action: PayloadAction<Offer[]>) => {
       state.offers = action.payload;
+    })
+    .addCase(setAuthorization, (state, action: PayloadAction<AuthorizationStatus>) => {
+      state.authStatus = action.payload;
     });
 });
