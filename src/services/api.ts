e976be52aafd693a0c4c6c61e6d0aa259ed20f0d
Irina Lagutina <ivl77@yandex.ru>
2024-08-28 20:11:07 +0300

    Тостер включила

--- src/services/api.ts
+++ src/services/api.ts
@@ -41,7 +41,7 @@ export const createAPI = (): AxiosInstance => {
       if (error.response && shouldDisplayError(error.response)) {
         const detailMessage = (error.response.data);
 
-        // toast.warn(detailMessage.message);
+        toast.warn(detailMessage.message);
       }
 
       throw error;
